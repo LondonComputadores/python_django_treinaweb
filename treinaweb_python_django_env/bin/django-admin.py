bc7c4786963e9d565a9dc80a05ff17bfb460a9f3
london <london.computadores@gmail.com>
2019-09-07 16:23:05 -0300

    Django project to_do_list_crud_proj criado.

---
+++ treinaweb_python_django_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/c/Users/londo/Documents/treinaweb/semana_python_django/treinaweb_python_django_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
