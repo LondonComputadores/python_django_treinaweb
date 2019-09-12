74496c20ebc71c6fa6197f4137818c08c73acaea
london <london.computadores@gmail.com>
2019-09-12 15:12:44 -0300

    Dockerizing my todo List app in a Virtualenv environment! Lol

--- to_do_list_crud_proj/to_do_list_crud_proj/settings.py
+++ to_do_list_crud_proj/to_do_list_crud_proj/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'to_do_list_crud_proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mydatabase',
     }
 }
 
